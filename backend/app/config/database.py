eaf5319092620b2ef6b45a249ea66bf753c17d57
xmeix <lamiaboualouache@gmail.com>
2023-08-16 19:36:08 +0100

    setting up mongodb connection

---
+++ backend/app/config/database.py
@@ -0,0 +1,22 @@
+import motor.motor_asyncio
+from decouple import config
+
+
+
+MONGO_URI = config("MONGO_URI")
+
+# Create a new client and connect to the server
+client = motor.motor_asyncio.AsyncIOMotorClient(MONGO_URI)
+
+
+db = client.todo_database
+
+users_collection = db["users"]
+
+
+# Send a ping to confirm a successful connection
+try:
+    client.admin.command('ping')
+    print("Pinged your deployment. You have successfully connected to MongoDB!")
+except Exception as e:
+    print(e)
